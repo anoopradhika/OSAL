dba2bc825ca765a774f3d30b54d0057a7b0b7bbc
anoopradhika <anoop.k.chandran@gmail.com>
2017-05-09 23:33:12 +0200

    Engine frame added

---
+++ Engine/Engine.cpp
@@ -0,0 +1,39 @@
+#include"Engine.h"
+
+Engine::Engine()
+{
+
+}
+
+~Engine::Engine()
+{
+
+}
+
+void Engine::start()
+{
+
+}
+
+void Engine::run()
+{
+
+}
+
+
+void Engine::stop()
+{
+
+}
+
+
+bool Engine::is_running()
+{
+
+}
+
+
+void Engine::off()
+{
+
+}
---
+++ Engine/Engine.h
@@ -0,0 +1,11 @@
+class Engine
+{
+public:
+  Engine();
+  ~Engine();
+
+  void start();
+  void run();
+  void stop();
+  void off();
+};
